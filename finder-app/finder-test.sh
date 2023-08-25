c1de96b1692106edc115d9fd6fed39a14814b13d
tfchaseGH <tchase@grayhill.com>
2023-08-25 16:55:06 -0400

    Use absolute paths to config files (Item 4.b.i of Assignment 4 Part 2).

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -10,7 +10,7 @@ WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 FINDER_APP_DIR=$(realpath $(dirname $0))
 cd $FINDER_APP_DIR
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -34,7 +34,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
