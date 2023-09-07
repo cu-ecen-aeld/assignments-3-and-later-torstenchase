6a58f3fd9a7481d34b4cc98e84f3a713be57cee8
tfchaseGH <tchase@grayhill.com>
2023-09-07 10:25:15 -0400

    Added daemon mode option changes.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -4,18 +4,26 @@
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 static volatile bool run = true;
 static void sig_handler(int signum);
 
 int main (int argc, char **argv) 
 {
+    bool dm = false;
+    if ((argc == 2) && (strcmp(argv[1], "-d") == 0))
+    {
+        syslog(LOG_DEBUG, "Will run aesdsocket as daemon");
+        dm = true;
+    }
     const char filename[] = "/var/tmp/aesdsocketdata";
     char* buf = NULL;
     FILE* writefile = NULL;
@@ -47,8 +55,8 @@ int main (int argc, char **argv)
         syslog(LOG_ERR, "Error getting addr info: %s", gai_strerror(status));
         goto error;
     }
-
     syslog (LOG_DEBUG, "sockaddr: %d: %d: %d, %s", res->ai_family, res->ai_socktype, res->ai_addr->sa_family, &res->ai_addr->sa_data[0]);
+
     if ((status = bind(sfd, res->ai_addr, sizeof(struct sockaddr))) != 0)
     {
         syslog(LOG_ERR, "Error binding socket: %s", strerror(errno));
@@ -58,128 +66,147 @@ int main (int argc, char **argv)
 
     freeaddrinfo(res);
 
-    struct sigaction act;
-    memset(&act, 0, sizeof(struct sigaction));
-    act.sa_handler = sig_handler;
-    if (sigaction(SIGTERM, &act, NULL) != 0)
+    pid_t pid = (dm) ? fork() : 0;
+
+    syslog(LOG_ERR, "pid: %d, sfd: %d", pid, sfd);
+    if (pid == 0)
     {
-        syslog(LOG_ERR, "Failed registering for SIGTERM: %s", strerror(errno));
-    }
-    if (sigaction(SIGINT, &act, NULL) != 0)
-    {
-        syslog(LOG_ERR, "Failed registering for SIGINT: %s", strerror(errno));
-    }
-    while (run)
-    {
-        if ((status = listen(sfd, 10)) != 0)
+        //child or non-daemon process
+        struct sigaction act;
+        memset(&act, 0, sizeof(struct sigaction));
+        act.sa_handler = sig_handler;
+        if (sigaction(SIGTERM, &act, NULL) != 0)
         {
-            syslog(LOG_ERR, "Error listening for connection: %s", strerror(errno));
+            syslog(LOG_ERR, "Failed registering for SIGTERM: %s", strerror(errno));
+            goto error;
+        }
+        if (sigaction(SIGINT, &act, NULL) != 0)
+        {
+            syslog(LOG_ERR, "Failed registering for SIGINT: %s", strerror(errno));
             goto error;
         }
 
-        struct sockaddr addr;
-        socklen_t addrlen = sizeof(struct sockaddr);
-        if ((afd = accept(sfd, &addr, &addrlen)) <= 0)
+        while (run)
         {
-            syslog(LOG_ERR, "Error accepting connection: %s", strerror(errno));
-            goto error;
-        }
-
-        char dst[64];
-        syslog(LOG_INFO, "Accepted connection from %s", inet_ntop(AF_INET, (struct sockaddr_in *)&addr, dst, sizeof(dst)));
-
-        writefile = fopen(filename, "a+");
-        if (!writefile)
-        {
-            syslog(LOG_ERR, "Could not open write file: %s", filename);
-            goto error;
-        }
-
-        
-        int sz;
-        buf = malloc(0x4000);
-        while ((sz = recv(afd, buf, 0x4000, 0)) > 0)
-        {
-            if (sz < 0) 
+            if ((status = listen(sfd, 10)) != 0)
             {
-                syslog(LOG_ERR, "Error while waiting for receive data: %s", strerror(errno));
+                syslog(LOG_ERR, "Error listening for connection: %s", strerror(errno));
                 goto error;
             }
-            if (fwrite(buf, 1, sz, writefile) < 1)
+
+            struct sockaddr addr;
+            socklen_t addrlen = sizeof(struct sockaddr);
+            if ((afd = accept(sfd, &addr, &addrlen)) <= 0)
             {
-                syslog(LOG_ERR, "Could not write %s to file", buf);
+                syslog(LOG_ERR, "Error accepting connection: %s", strerror(errno));
                 goto error;
             }
-            syslog(LOG_DEBUG, "Flushing to file");
-            fflush(writefile);
 
-            if (buf[sz-1] == '\n')
+            char dst[64];
+            syslog(LOG_INFO, "Accepted connection from %s", inet_ntop(AF_INET, (struct sockaddr_in *)&addr, dst, sizeof(dst)));
+
+            writefile = fopen(filename, "a+");
+            if (!writefile)
             {
-                break;
+                syslog(LOG_ERR, "Could not open write file: %s", filename);
+                goto error;
             }
-        }
 
-        free(buf);
-        buf = NULL;
-        if (fclose(writefile) != 0)
-        {
-            syslog(LOG_ERR, "Could not close write file: %s", filename);
-            writefile = NULL;
-            goto error;
-        }
-        writefile = NULL;
-
-        syslog(LOG_DEBUG, "Opening readfile");
-        readfile = fopen(filename, "rb");
-        if (!readfile)
-        {
-            syslog(LOG_ERR, "Could not open read file: %s", filename);
-            goto error;
-        }
-
-        size_t j;
-        while (!feof(readfile))
-        {
-            int tsz;
-            j = 0;
-            sz = getline(&buf, &j, readfile);
-            if (sz < 0)
+            
+            int sz;
+            buf = malloc(0x4000);
+            while ((sz = recv(afd, buf, 0x4000, 0)) > 0)
             {
-                syslog(LOG_DEBUG, "Could not get another line from file %s, error: %s", filename, strerror(errno));
-            }
-            else
-            {
-                syslog(LOG_DEBUG, "Writing %d characters: %s to socket", sz, buf);
-                tsz = send(afd, buf, sz, 0);
-                if (tsz != sz)
+                if (sz < 0) 
                 {
-                    syslog(LOG_ERR, "Buffer length mismatch #3");
+                    syslog(LOG_ERR, "Error while waiting for receive data: %s", strerror(errno));
                     goto error;
                 }
-                syslog(LOG_DEBUG, "Done Writing %d characters: %s to socket", sz, buf);
+                if (fwrite(buf, 1, sz, writefile) < 1)
+                {
+                    syslog(LOG_ERR, "Could not write %s to file", buf);
+                    goto error;
+                }
+                syslog(LOG_DEBUG, "Flushing to file");
+                fflush(writefile);
+
+                if (buf[sz-1] == '\n')
+                {
+                    break;
+                }
             }
+
             free(buf);
             buf = NULL;
-        }
-        syslog(LOG_INFO, "Found EOF in %s", filename);
-        if (fclose(readfile) != 0)
-        {
-            syslog(LOG_ERR, "Could not close read file: %s", filename);
+            if (fclose(writefile) != 0)
+            {
+                syslog(LOG_ERR, "Could not close write file: %s", filename);
+                writefile = NULL;
+                goto error;
+            }
+            writefile = NULL;
+
+            syslog(LOG_DEBUG, "Opening readfile");
+            readfile = fopen(filename, "rb");
+            if (!readfile)
+            {
+                syslog(LOG_ERR, "Could not open read file: %s", filename);
+                goto error;
+            }
+
+            size_t j;
+            while (!feof(readfile))
+            {
+                int tsz;
+                j = 0;
+                sz = getline(&buf, &j, readfile);
+                if (sz < 0)
+                {
+                    syslog(LOG_DEBUG, "Could not get another line from file %s, error: %s", filename, strerror(errno));
+                }
+                else
+                {
+                    syslog(LOG_DEBUG, "Writing %d characters: %s to socket", sz, buf);
+                    tsz = send(afd, buf, sz, 0);
+                    if (tsz != sz)
+                    {
+                        syslog(LOG_ERR, "Buffer length mismatch #3");
+                        goto error;
+                    }
+                    syslog(LOG_DEBUG, "Done Writing %d characters: %s to socket", sz, buf);
+                }
+                free(buf);
+                buf = NULL;
+            }
+            syslog(LOG_INFO, "Found EOF in %s", filename);
+            if (fclose(readfile) != 0)
+            {
+                syslog(LOG_ERR, "Could not close read file: %s", filename);
+                readfile = NULL;
+                goto error;
+            }
             readfile = NULL;
-            goto error;
+
+            shutdown(afd, SHUT_RDWR);
+            close(afd);
+            syslog(LOG_INFO, "Closed connection from %s", inet_ntop(AF_INET, (struct sockaddr_in *)&addr, dst, sizeof(dst)));
         }
-        readfile = NULL;
 
-        shutdown(afd, SHUT_RDWR);
-        close(afd);
-        syslog(LOG_INFO, "Closed connection from %s", inet_ntop(AF_INET, (struct sockaddr_in *)&addr, dst, sizeof(dst)));
+        shutdown(sfd, SHUT_RDWR);
+        close(sfd);
+        remove(filename);
+        closelog();
+        exit(EXIT_SUCCESS);
+
     }
-
-    shutdown(sfd, SHUT_RDWR);
-    close(sfd);
-    remove(filename);
-    closelog();
-    return 0;
+    else if (pid > 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        exit(EXIT_FAILURE);
+    }
 
 error:
     remove(filename);
